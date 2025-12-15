fc059aa18ef27653b09002feb13c91a418b2c92e
julioOv-1104 <julioovalle202232237@cunoc.edu.gt>
2025-12-15 00:08:08 -0600

    creo que resolvi el problema

---
+++ ProyectoVaquerasIPC2/src/main/java/Logica/LogicaEmpresa.java
@@ -0,0 +1,54 @@
+package Logica;
+
+import DAOs.*;
+import Entidades.*;
+
+public class LogicaEmpresa {
+
+    private  EmpresaDAO empresaDAO = new EmpresaDAO();
+
+
+
+    public Empresa registrar(Empresa nuevo) {       
+            return empresaDAO.registrarEmpresa(nuevo);
+    }
+    
+    public boolean cambiarComisionAEspecifica(double nuevaComision, String empresa){
+    
+        if (nuevaComision > empresaDAO.obtenerComisionGlobal()) {
+            return false;
+            
+            
+        }else{
+        return empresaDAO.cambiarComisionEspecifica(nuevaComision, empresa);
+        }
+    }
+    
+    public boolean actualizarComision(double nuevaComision){
+    
+        if (nuevaComision >0) {
+            return empresaDAO.cambiarComisionGlobalParaEmpresas(nuevaComision);
+        }else{
+        return false;
+        }
+        
+    }
+    
+    public void cambiarCategoriaJuego(String juego, String nueva, String vieja){
+    
+        empresaDAO.cambiarCategoriaJuego(juego, nueva, vieja);
+        
+    }
+    
+    public boolean desactivarActivarComenatrio(String empresa){
+    
+        return empresaDAO.cambiarVisibilidadComentariosEmpresa(empresa);
+        
+    }
+    
+    public boolean editarInformacion(String titulo, String descripcion, double precio){
+    
+        return empresaDAO.editarInformacion(titulo, descripcion, precio);
+        
+    }
+}
---
+++ ProyectoVaquerasIPC2/src/main/java/Logica/LogicaUsuario.java
@@ -0,0 +1,28 @@
+package Logica;
+
+import DAOs.UsuarioDAO;
+import Entidades.Usuario;
+import Utilidades.TipoUsuarioENUM;
+
+public class LogicaUsuario {
+
+    private  UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+    public Usuario login(String correo_usuario, String contrasenna) {
+        return usuarioDAO.login(correo_usuario, contrasenna);
+    }
+
+    public Usuario registrar(Usuario nuevo) {
+
+        if (nuevo.getTipo_usuario().equals(TipoUsuarioENUM.ADMIN)) {
+            return usuarioDAO.registrarAdmin(nuevo);
+            
+        } else if (nuevo.getTipo_usuario().equals(TipoUsuarioENUM.USUARIO_COMUN)) {
+            return usuarioDAO.registrarUsuario(nuevo);
+            
+        }else if (nuevo.getTipo_usuario().equals(TipoUsuarioENUM.EMPRESARIO)) {
+            return usuarioDAO.registrarEmpresario(nuevo);
+        }
+        return null;
+    }
+}
